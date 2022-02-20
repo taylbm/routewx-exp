9bfdc01299681372a705117973386058196c3bfb
Brandon Taylor <brandontaylor@Brandons-iMac.local>
2022-02-19 18:15:00 -0700

    add ga tracking -bt

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -41,6 +41,23 @@ class MyDocument extends Document {
           <meta property='og:site_name' content='' />
           <meta property='og:url' content='' key='og:url' />
           <meta property='og:image' content='' key='og:image' />
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </Head>
 
         <body className='h-full'>
